f1d5a2ba08b58537c2520462b3bc39d5d2320557
gustavoborro <72629529+gustavoborro@users.noreply.github.com>
2026-06-08 14:50:09 -0300

    Add files via upload

---
+++ coleta.ps1
@@ -0,0 +1,17 @@
+$Inventory = @{
+    ComputerName = $env:COMPUTERNAME
+    UserName     = $env:USERNAME
+    Domain       = $env:USERDOMAIN
+    OS           = (Get-CimInstance Win32_OperatingSystem).Caption
+    Version      = (Get-CimInstance Win32_OperatingSystem).Version
+    MemoryGB     = [math]::Round((Get-CimInstance Win32_ComputerSystem).TotalPhysicalMemory / 1GB, 2)
+    Timestamp    = (Get-Date).ToString("o")
+}
+
+$json = $Inventory | ConvertTo-Json -Depth 5
+
+Invoke-RestMethod `
+    -Uri "http://20.206.201.113:8080" `
+    -Method POST `
+    -Body $json `
+    -ContentType "application/json"
